e01f87d9ce951190a9e07e5ca58fd412db25d954
fakhrovski <Fakhrovski@DESKTOP-LDMK5KF>
2019-03-12 16:27:15 +0100

    Add Firebase Auth components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// App routing modules
+import { AppRoutingModule } from './shared/routing/app-routing.module';
 
+// Firebase services + enviorment module
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+// Auth service
+import { AuthService } from "./shared/services/auth.service";
+
+
+//Components
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
 
-
+//Environment
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDXZutsU7H5EpVrC-zDt2EoFqkrrxaVXIs",
+    authDomain: "quiz-app-6d0b2.firebaseapp.com",
+    databaseURL: "https://quiz-app-6d0b2.firebaseio.com",
+    projectId: "quiz-app-6d0b2",
+    storageBucket: "quiz-app-6d0b2.appspot.com",
+    messagingSenderId: "279817216277"
+  }
 };
