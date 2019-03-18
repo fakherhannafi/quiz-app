44d24d356e0863509fdfdcf985367769ec786c76
fakhrovski <Fakhrovski@DESKTOP-LDMK5KF>
2019-03-18 13:49:46 +0100

    cleaning code

--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -10,16 +10,16 @@ import { AuthService } from "../../shared/services/auth.service";
 export class AuthGuard implements CanActivate {
   constructor(
     public authService: AuthService,
-    public router: Router 
-  ){}
+    public router: Router
+  ) { }
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     if (this.authService.isLoggedIn) {
       window.alert("You are not allowed to access this URL!");
       this.router.navigate(['dashboard'])
     }
-      return true;
+    return true;
   }
 
 }
